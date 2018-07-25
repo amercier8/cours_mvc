638a7af0ba2619f2fcbbd68ef5054ad99e5ba3c1
amercier8 <a.mercier8@gmail.com>
2018-07-25 13:29:30 +0200

    entities initialized

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -1,8 +1,10 @@
 <?php
 
+//Revoir le concept de namespace en profondeur
 namespace OpenClassrooms\Blog\Model;
 
 require_once("model/Manager.php");
+require_once("model/entities/Comment.php");
 
 class CommentManager extends Manager
 {
@@ -30,6 +32,7 @@ class CommentManager extends Manager
         $db = $this->dbConnect();
         $req = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE id = ? ORDER BY comment_date DESC');
         $req->execute(array($commentId));
+        
         $toto = $req->fetch();
 
         return $toto;
---
+++ model/entities/Comment.php
@@ -0,0 +1,67 @@
+<?php
+class Comment {
+    private $postId;
+    private $id, $author, $comment, $commenDate;
+
+    //Function hydrate to add
+    public function hydrate (array $donnees) {
+        foreach ($donnees as $key => $value) {
+            // get the "setter" name corresponding to the attribute
+            $method = 'set'.ucfirst($key);
+            //Boolean test on the $method parameter
+            if (method_exists($this, $method)) {
+                //$method being the name of the parameter to set
+                $this->$method($value);
+            }
+        }
+    }
+
+    //constructor to add
+    public function __construct(array $donnees) {
+        $this->hydrate($donnees);
+    }
+
+    //Setters
+    //
+    //Manque les contrôles sur les données
+    //
+    public function setPostId($postId) {
+        $this->postId = $postId;
+    }
+
+    public function setAuthor($author) {
+        $this->author = $author;
+    }
+
+    public function setComment($comment) {
+        $this->comment = $comment;
+    }
+
+    public function setCommentDate($commentDate) {
+        $this->commentDate = $commentDate;
+    }
+
+
+    //Getters
+    public function getPostID() {
+        return $this->postId;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getAuthor() {
+        return $this->author;
+    }
+
+    public function getComment() {
+        return $this->comment;
+    }
+
+    public function getCommentDate() {
+        return $this->commentDate;
+    }
+
+
+}
