3a9bdf003dbfe36b7f816c0346dc299e716f9649
amercier8 <a.mercier8@gmail.com>
2018-08-01 08:22:59 +0200

    PostManager back to normal

--- model/PostManager.php
+++ model/PostManager.php
@@ -12,6 +12,7 @@ class PostManager extends Manager
 
         //TESTS
         //Connexion to the DB
+        /*
         $db = $this->dbConnect();
         //$sql contains the SQL request
         $sql = 'SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5';
@@ -22,17 +23,18 @@ class PostManager extends Manager
             $post = new Post($result);
             array_push($posts, $post);
         }
-
+        */
         //FIN DES TESTS
-        /*
+        
         $db = $this->dbConnect();
         $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
-        */
+        
         return $posts;
     }
 
     public function getPost($postId)
     {
+        /*
         $db = $this->dbConnect();
         $sql = 'SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?';
         $results = $this->executeRequest($sql);
@@ -41,13 +43,13 @@ class PostManager extends Manager
             $post = new Post($result);
             array_push($posts, $post);
         }
-
-        /*
+        */
+        
         $db = $this->dbConnect();
         $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
         $req->execute(array($postId));
         $post = $req->fetch();
-        */
+        
         return $post;
     }
 }
